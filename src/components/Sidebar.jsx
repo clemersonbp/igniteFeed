bf83e9d292e953f43f6b923874c781c1b784dd6b
clemersonbp <clemersonpessalli@live.com>
2022-07-22 23:04:56 -0300

    Create side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -5,11 +6,13 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcG1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=50" alt="" />
       <div className={styles.profile}>
-        <strong>Diego Fernandes</strong>
+        <img className={styles.avatar} src="https://github.com/clemersonbp.png" />
+        <strong>Clemerson Pessalli</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
