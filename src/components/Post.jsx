e5380e0fb752323923e5aae2f9db8a98406a375c
clemersonbp <clemersonpessalli@live.com>
2022-08-05 01:38:28 -0300

    Estrutura do projeto finalizada.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,6 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/clemersonbp.png" />
+          <Avatar src="https://github.com/clemersonbp.png" />
           <div className={styles.authorInfo}>
             <strong>Clemerson Pessalli</strong>
             <span>Web Developer</span>
@@ -33,6 +36,12 @@ export function Post() {
           <button type="submit" >Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -6,7 +7,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcG1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=50" alt="" />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/clemersonbp.png" />
+        <Avatar src="https://github.com/clemersonbp.png" />
         <strong>Clemerson Pessalli</strong>
         <span>Web Developer</span>
       </div>
